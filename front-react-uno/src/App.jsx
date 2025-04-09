16c2a6e54eff462c286a04e74ed7caa88d79eef0
daferarte <daferarte@gmail.com>
2025-04-08 19:44:27 -0500

    react basico

---
+++ front-react-uno/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Saludo from './components/Saludo';
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Mi primer proyecto en React</h1>
+      <Saludo nombre="Daniel" />
+    </div>
+  )
+}
+
+export default App
---
+++ front-react-uno/src/components/Saludo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Saludo({nombre}){
+    return (
+        <h2>hola, {nombre} !Bienvenido a React</h2>
+    );
+}
+
+export default Saludo;
